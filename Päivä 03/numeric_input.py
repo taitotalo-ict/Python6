2cb297c8bb07c95994f195e521697116a98f53ad
Christian Finnberg <christian.finnberg@taitotalo.fi>
2024-11-25 10:22:44 +0200

    20241125_102244

---
+++ Päivä 03/numeric_input.py
@@ -0,0 +1,27 @@
+# Kysy jostain numerosta
+# Tarkista onko numeerinen vastaus
+# Kysy uudelleen jos ei ole
+# Muunna kokonaisluvuksi jos vastaus on num.
+
+# While/For, input, int, if
+
+is_not_number = True
+while is_not_number:
+    # Kysy
+    vastaus = input('Ikä? ')
+    if not vastaus:
+        continue
+
+    is_not_number = False
+    # Tarkista
+    for character in vastaus:
+        # if character >= '0' and character <= '9':
+        # if '0' <= character <= '9':
+        if character not in '0123456789':
+            is_not_number = True
+            print('Vastaus ei ole numero')
+            break
+
+ikä = int(vastaus)
+
+
