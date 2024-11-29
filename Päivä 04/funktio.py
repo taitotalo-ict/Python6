23e02cfeb3fbb8e7e2ea3d5f81ee29f818d00926
Christian Finnberg <christian.finnberg@taitotalo.fi>
2024-11-29 13:46:58 +0200

    20241129_134658

---
+++ Päivä 04/funktio.py
@@ -0,0 +1,14 @@
+def greet(name, age):
+    print('Moi', name)
+    print('Sinun ikasi on', age)
+
+greet('Christian', 50)
+greet('Simo', 25)
+#greet(20, 'Anna')
+#greet()
+
+def laske_summa(luku1, luku2):
+    return luku1 + luku2
+
+laske_summa(3, 5)
+print(laske_summa(1, 2))
--- Päivä 04/ksp.py
+++ Päivä 04/ksp.py
@@ -6,33 +6,35 @@ from random import choice
 OPTIONS = ('kivi', 'sakset', 'paperi')
 OPTIONS_QUIT = ('quit', 'stop', 'end', 'q', 'x')
 
-
-while True:
-    # Saada käyttäjän valinta
+def get_player_option():
     while True:
         player_option = input('Kivi, sakset vai paperi? ')
 
-        # if player_option == 'k':
-        #     player_option = 'kivi'
-        #     break
-        # if player_option == 's':
-        #     player_option = 'sakset'
-        #     break
-        # if player_option == 'p':
-        #     player_option = 'paperi'
-        #     break
-
         for option in OPTIONS:
             if player_option == option[0]:
-                player_option = option
+                return option
 
         if player_option in OPTIONS:
-            break
+            return player_option
 
         if player_option in OPTIONS_QUIT:
-            player_option = None
-            break
-    
+            return None
+
+def get_winner_msg(player_option, computer_option):
+    player_index = OPTIONS.index(player_option)
+    computer_index = OPTIONS.index(computer_option)
+
+    if player_index == computer_index:
+        return 'Tasapeli!'
+    elif computer_index == (player_index + 1) % 3:
+        return 'Voitit!'
+    else:
+        return 'Kone voitti!'
+
+
+while True:
+    # Saada käyttäjän valinta
+    player_option = get_player_option()
     if player_option is None:
         break
     
@@ -41,38 +43,6 @@ while True:
     print(f'Kone valitse "{computer_option}"')
 
     # Laskea voittaja niiden perustella
-    player_index = OPTIONS.index(player_option)
-    computer_index = OPTIONS.index(computer_option)
-
-    if player_index == computer_index:
-        print('Tasapeli!')
-    elif computer_index == (player_index + 1) % 3:
-        print('Voitit!')
-    else:
-        print('Kone voitti!')
-
-    # if computer_option == player_option:
-    #     print('Tasapeli!')
-    # # elif player_option == 'kivi':
-    # #     if computer_option == 'sakset':
-    # #         print('Voitit!')
-    # #     else:
-    # #         print('Kone voitti!')
-    # # elif player_option == 'sakset':
-    # #     if computer_option == 'paperi':
-    # #         print('Voitit!')
-    # #     else:
-    # #         print('Kone voitti!')
-    # # else:
-    # #     if computer_option == 'kivi':
-    # #         print('Voitit!')
-    # #     else:
-    # #         print('Kone voitti!')
-    # elif (player_option == 'kivi' and computer_option == 'sakset') \
-    #    or (player_option == 'sakset' and computer_option == 'paperi') \
-    #    or (player_option == 'paperi' and computer_option == 'kivi'):
-    #     print('Voitit!')
-    # else:
-    #     print('Kone voitti!')
+    print(get_winner_msg(player_option, computer_option))
 
 print('Kiitos pelaamisesta')
