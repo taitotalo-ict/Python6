17970fdc6e6c3d4a038011daae8e17206223e72b
Christian Finnberg <christian.finnberg@taitotalo.fi>
2024-11-29 14:31:42 +0200

    20241129_143142

--- Päivä 04/funktio.py
+++ Päivä 04/funktio.py
@@ -1,14 +1,27 @@
-def greet(name, age):
-    print('Moi', name)
-    print('Sinun ikasi on', age)
+# def greet(name, age):
+#     print('Moi', name)
+#     print('Sinun ikasi on', age)
 
-greet('Christian', 50)
-greet('Simo', 25)
-#greet(20, 'Anna')
-#greet()
+# greet('Christian', 50)
+# greet('Simo', 25)
+# #greet(20, 'Anna')
+# #greet()
 
-def laske_summa(luku1, luku2):
-    return luku1 + luku2
+# def laske_summa(luku1, luku2):
+#     return luku1 + luku2
 
-laske_summa(3, 5)
-print(laske_summa(1, 2))
+# laske_summa(3, 5)
+# print(laske_summa(1, 2))
+
+def A():
+    global i
+    i = 3
+    print(i)
+    l[0] = 10
+    print(l)
+
+l = [1, 2, 3]
+i = 5
+A()
+print(i)
+print(l)
