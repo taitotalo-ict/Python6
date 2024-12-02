99f36a2295bec16a8b0079cb4ea7e1ca3bfcc473
Christian Finnberg <christian.finnberg@taitotalo.fi>
2024-12-02 15:14:05 +0200

    20241202_151405

--- Päivä 05/helper.py
+++ Päivä 05/helper.py
@@ -1,17 +1,19 @@
-def _is_number(string):
+"""Custom helper functions to be used in different situations"""
+
+def _is_number(string: str) -> bool:
+    """Checks that string is composed only with number chars"""
     for char in string:
         if char not in '0123456789':
             return False
     return True
 
-def input_int(question):
+def input_int(question: str='') -> int:
+    """Asks user using the question and accepts only 
+    an integer number as answer.
+    Returns the integer version of the answer"""
     while True:
         answer = input(question)
         if not answer:
             continue
         if _is_number(answer):
             return int(answer)
-
-
-
-
