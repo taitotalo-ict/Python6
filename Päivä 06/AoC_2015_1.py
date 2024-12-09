bb813381b5ce10386d9c141a0914b31e7f5897c2
Christian Finnberg <christian.finnberg@taitotalo.fi>
2024-12-09 14:38:47 +0200

    20241209_143847

--- Päivä 06/AoC_2015_1.py
+++ Päivä 06/AoC_2015_1.py
@@ -5,16 +5,16 @@ input_str = '((((()(()(((((((()))(((()((((()())(())()(((()((((((()((()(()(((()((
 
 # Osa 1: Mihin kerrokseen Santa päätyy?
 
-floor = 0
-for char in input_str:
-    if char == '(':
-        floor += 1 # floor = floor + 1
-    else:
-        floor -= 1
+# floor = 0
+# for char in input_str:
+#     if char == '(':
+#         floor += 1 # floor = floor + 1
+#     else:
+#         floor -= 1
     
 
-floor = input_str.count('(') - input_str.count(')')
-print('Osa 1:', floor)
+# floor = input_str.count('(') - input_str.count(')')
+# print('Osa 1:', floor)
 
 # Osa 2: Missä ohjeiden sijainnissa se päätyy kerrokseen -1?
 
@@ -45,15 +45,42 @@ print('Osa 1:', floor)
 
 # print('Osa 2:', position)
 
+# floor = 0
+# for index, char in enumerate(input_str):
+#     if char == '(':
+#         floor += 1 # floor = floor + 1
+#     else:
+#         floor -= 1
+
+#     if floor == -1:
+#         break
+
+# position = index + 1
+# print('Osa 2:', position)
+
+# floor = 0
+# position = 0
+# while floor != -1:
+#     char = input_str[position]
+#     position += 1
+#     if char == '(':
+#         floor += 1 # floor = floor + 1
+#     else:
+#         floor -= 1
+
+# print('Osa 2:', position)
+
+
 floor = 0
+position = None
 for index, char in enumerate(input_str):
     if char == '(':
         floor += 1 # floor = floor + 1
     else:
         floor -= 1
+    
+    if position is None and floor == -1:
+        position = index + 1
 
-    if floor == -1:
-        break
-
-position = index + 1
+print('Osa 1:', floor)
 print('Osa 2:', position)
