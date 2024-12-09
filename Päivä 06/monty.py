b8fdeb00bbd72c6de7a0fd94e86cf65bfd7331bb
Christian Finnberg <christian.finnberg@taitotalo.fi>
2024-12-09 10:15:37 +0200

    20241209_101537

--- Päivä 06/monty.py
+++ Päivä 06/monty.py
@@ -1,8 +1,9 @@
 from random import shuffle, randint
 
-def play(verbose:bool) -> bool:
+def play(verbose:bool, change:bool = True) -> bool:
     options = ['Goat', 'Goat', 'Car']
     shuffle(options)
+
     # Pelaajan valinta
     player_option = randint(0, 2)
     if verbose:
@@ -19,23 +20,19 @@ def play(verbose:bool) -> bool:
     if verbose:
         print(f'Jos pelaaja ei vaihta ovea, se saisi {options[player_option]}')
     
-    player_option = 3 - player_option - monty_option
-    if verbose:
-        print(f'Jos pelaaja vaihtaa oveen {player_option+1}, se saisi {options[player_option]}')
+    if change:
+        player_option = 3 - player_option - monty_option
+        if verbose:
+            print(f'Jos pelaaja vaihtaa oveen {player_option+1}, se saisi {options[player_option]}')
 
-    # if options[player_option] == 'Car':
-    #     return True
-    # else:
-    #     return False
-    
     return options[player_option] == 'Car'
 
 if __name__ == '__main__':
     # play(verbose=True)
     wins = 0
-    rounds = 100000
+    rounds = 1000000
     for _ in range(rounds):
-        if play(verbose=False):
+        if play(verbose=False, change=True):
             wins += 1
 
     print(f'Pelaaja on voitanut {wins/rounds * 100}% kertaa')
