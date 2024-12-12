47008b934175a4673cf6e612b015c63495f2a777
Christian Finnberg <christian.finnberg@taitotalo.fi>
2024-12-12 12:46:43 +0200

    20241212_124643

--- Päivä 07/AoC_2015_2.py
+++ Päivä 07/AoC_2015_2.py
@@ -1,20 +1,13 @@
-
 file = open(r'S:\Christian Finnberg\Ohjelmointi\Python\AoC\2015\input_2015_2.txt', 'r')
-#input_lista = tuple(map(str.rstrip, file.readlines()))
-
-# input_lista: list[str] = []
-# for line in file.readline():
-#     input_lista.append(line.rstrip())
-
-input_lista = file.read().rstrip().split()
-
 
 paper = 0
 ribbon = 0
-for line in input_lista:
-    l, w, h = map(int, line.split('x'))
+for line in file.readlines():
+    l, w, h = map(int, line.rstrip().split('x'))
     paper += 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
     ribbon += min(2*l+2*w, 2*w+2*h, 2*l+2*h) + (l*w*h)
+
+file.close()
 
 print('Part 1:', paper)
 print('Part 2:', ribbon)
