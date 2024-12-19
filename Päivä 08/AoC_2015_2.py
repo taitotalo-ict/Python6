796d20355e9463509a1c4e8f54f5983727ec13d3
Christian Finnberg (opiskelijana) <christian.finnberg@tite.amiedu-ict.net>
2024-12-19 14:55:57 +0200

    20241219_145557

--- Päivä 08/AoC_2015_2.py
+++ Päivä 08/AoC_2015_2.py
@@ -1,12 +1,17 @@
 with open(r'S:\Christian Finnberg\Ohjelmointi\Python\AoC\2015\input_2015_2.txt', 'r') as file:
-    file_content = file.readlines()
+    data = [tuple(map(int, line.rstrip().split('x'))) for line in file.readlines()]
 
-paper = 0
-ribbon = 0
-for line in file_content:
-    l, w, h = map(int, line.rstrip().split('x'))
-    paper += 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
-    ribbon += min(2*l+2*w, 2*w+2*h, 2*l+2*h) + (l*w*h)
+# paper = 0
+# for l, w, h in data:
+#     paper += 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
+
+
+# ribbon = 0
+# for l, w, h in data:
+#     ribbon += min(2*l+2*w, 2*w+2*h, 2*l+2*h) + (l*w*h)
+
+paper = sum(2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l) for l, w, h in data)
+ribbon = sum(min(2*l+2*w, 2*w+2*h, 2*l+2*h) + (l*w*h) for l, w, h in data)
 
 print('Part 1:', paper)
 print('Part 2:', ribbon)
