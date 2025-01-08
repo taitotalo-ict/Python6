e35b2ebf3d107803f4924dd559a00372e8ab52ab
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-01-08 14:20:23 +0200

    20250108_142023

--- Päivä 10/AoC_2024_2.py
+++ Päivä 10/AoC_2024_2.py
@@ -16,9 +16,9 @@ with open(BASEDIR / 'input_2024_2.txt', 'r') as file:
 #             return False
 #     return True
 
-def check_increasing(line: tuple[int, ...]) -> bool:
-    pairs = [(line[index-1], line[index]) for index in range(1, len(line))]
-    return all(map(lambda pair: pair[1] > pair[0], pairs))
+# def check_increasing(line: tuple[int, ...]) -> bool:
+#     pairs = [(line[index-1], line[index]) for index in range(1, len(line))]
+#     return all(map(lambda pair: pair[1] > pair[0], pairs))
 
 # def check_decreasing(line: tuple[int, ...]) -> bool:
 #     for index in range(1, len(line)):
@@ -26,9 +26,9 @@ def check_increasing(line: tuple[int, ...]) -> bool:
 #             return False
 #     return True
 
-def check_decreasing(line: tuple[int, ...]) -> bool:
-    pairs = [(line[index-1], line[index]) for index in range(1, len(line))]
-    return all(map(lambda pair: pair[1] < pair[0], pairs))
+# def check_decreasing(line: tuple[int, ...]) -> bool:
+#     pairs = [(line[index-1], line[index]) for index in range(1, len(line))]
+#     return all(map(lambda pair: pair[1] < pair[0], pairs))
 
 # def check_diff(line: tuple[int, ...]) -> bool:
 #     for index in range(1, len(line)):
@@ -36,9 +36,9 @@ def check_decreasing(line: tuple[int, ...]) -> bool:
 #             return False
 #     return True
 
-def check_diff(line: tuple[int, ...]) -> bool:
-    pairs = [(line[index-1], line[index]) for index in range(1, len(line))]
-    return all(map(lambda pair: abs(pair[1] - pair[0]) <= 3, pairs))
+# def check_diff(line: tuple[int, ...]) -> bool:
+#     pairs = [(line[index-1], line[index]) for index in range(1, len(line))]
+#     return all(map(lambda pair: abs(pair[1] - pair[0]) <= 3, pairs))
 
 # def is_safe(line: tuple[int, ...]) -> bool:
 #     pairs = [(line[index-1], line[index]) for index in range(1, len(line))]
@@ -72,6 +72,15 @@ def is_safe(line: tuple[int, ...]) -> bool:
 #     if is_safe(line):
 #         count += 1
 
-count = sum(is_safe(line) for line in data)
+print('Part 1:', sum(is_safe(line) for line in data))
 
-print(count)
+# def is_safe_removing(line: tuple[int, ...]) -> bool:
+#     for index in range(len(line)):
+#         if is_safe(line[:index] + line[index+1:]):
+#             return True
+#     return False
+
+def is_safe_removing(line: tuple[int, ...]) -> bool:
+    return any(is_safe(line[:index] + line[index+1:]) for index in range(len(line)))
+
+print('Part 2:', sum(is_safe_removing(line) for line in data))
