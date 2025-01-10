9c26bb869e93ffb9b5eecbbd76f6de02a6df64d6
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-01-10 13:16:27 +0200

    20250110_131627

--- Päivä 11/AoC_2024_3.py
+++ Päivä 11/AoC_2024_3.py
@@ -4,15 +4,20 @@ import re
 BASEDIR = Path(__file__).parent
 
 with open(BASEDIR / 'input_2024_3.txt') as file:
+#    text = file.read().replace('\n', '')
     text = file.read()
 
-result = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', text)
+def sum_muls(text: str) -> int:
+    result = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', text)
+    return sum(int(a)*int(b) for a, b in result)
 
 # total = 0
 # for elem in result:
 #     total += int(elem[0])*int(elem[1])
 
 # total = sum(int(elem[0])*int(elem[1]) for elem in result)
-total = sum(int(a)*int(b) for a, b in result)
 
-print('Part 1: ', total)
+print('Part 1: ', sum_muls(text))
+
+text2 = re.sub(r"don't\(\).*?(?:do\(\)|$)", '', text, flags=re.DOTALL)
+print('Part 2: ', sum_muls(text2))
