14cd5bd6207465dcc492c79472acdf7fcd4473ba
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-01-20 13:29:16 +0200

    20250120_132915

--- Olio1/Harjoitus3.py
+++ Olio1/Harjoitus3.py
@@ -2,17 +2,21 @@ class Student:
     def __init__(self, first_name, last_name) -> None:
         self.first_name = first_name
         self.last_name = last_name
-        self.completed_courses = []
+        self._completed_courses = []
+        # self.full_name = f'{self.first_name} {self.last_name}'
     
+    def _get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
     def add_course(self, course):
-        self.completed_courses.append(course)
+        self._completed_courses.append(course)
 
     def print_courses(self):
-        if len(self.completed_courses) == 0:
-            print(f'Student {self.first_name} {self.last_name} has not completed any courses yet')
+        if len(self._completed_courses) == 0:
+            print(f'Student {self._get_full_name()} has not completed any courses yet')
         else:
-            print(f'Student {self.first_name} {self.last_name} has completed the following courses:')
-            for course in self.completed_courses:
+            print(f'Student {self._get_full_name()} has completed the following courses:')
+            for course in self._completed_courses:
                 print(f'- {course}')
 
 christian = Student('Christian', 'Finnberg')
@@ -20,3 +24,4 @@ christian.print_courses()
 christian.add_course('Python')
 christian.add_course('Linux')
 christian.print_courses()
+print(christian._get_full_name())
