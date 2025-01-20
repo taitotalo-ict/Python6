e78a4671bb9de064066be8f8002f8c21e614b016
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-01-20 13:00:07 +0200

    20250120_130006

--- Olio1/Harjoitus1.py
+++ Olio1/Harjoitus1.py
@@ -3,16 +3,15 @@ class Car:
         self.brand = brand
         self.color = color
         self.speed = speed
-        self.info = f'Brand: {brand}\nColor: {color}\nSpeed: {speed}'
-        self.info = 'Brand: ' + brand + '\nColor: ' + color + '\nSpeed: ' + str(speed)
-        self.info = f'''
-Brand: {brand}
-Color: {color}
-Speed: {speed}'''
+
+    def info(self):
+        # print(f'Brand: {self.brand}\nColor: {self.color}\nSpeed: {self.speed}')
+        return f'Brand: {self.brand}\nColor: {self.color}\nSpeed: {self.speed}'
+
 
 audi = Car('Audi', 'red', 200)
 skoda = Car('Skoda', 'Blue', 160)
 
-print(audi.info)
-
-print(skoda.info)
+print(audi.info())
+audi.color = 'black'
+print(audi.info())
