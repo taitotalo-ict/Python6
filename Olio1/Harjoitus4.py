bc921a6085eeda54ada271bcd8c23f52551f2454
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-01-21 09:36:34 +0200

    20250121_093634

--- Olio1/Harjoitus4.py
+++ Olio1/Harjoitus4.py
@@ -1,3 +1,19 @@
+class Guessword:
+    def __init__(self, word: str) -> None:
+        self._word = word
+        self._resolved_word = ['_'] * len(word)
+
+    def is_resolved(self) -> bool:
+        return '_' not in self._resolved_word
+
+    def hidden_word(self) -> str:
+        return ''.join(self._resolved_word)
+    
+    def resolve_letter(self, letter: str) -> None:
+        for index, word_letter in enumerate(self._word):
+            if word_letter == letter.lower():
+                self._resolved_word[index] = letter.lower()
+
 game = Guessword('kuningas')
 
 while not game.is_resolved():
