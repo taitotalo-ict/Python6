aa93e772e2f11e9f9d420f9d02fd09ce5ac7ea48
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-01-27 11:00:44 +0200

    20250127_110044

--- Olio4/harjoitus1.py
+++ Olio4/harjoitus1.py
@@ -32,10 +32,6 @@ class Point:
         else:
             raise TypeError('Wrong types for Point')
 
-    def _init_with_integers(self, x, y):
-        self.x = x
-        self.y = y
-
 p1 = Point(4, 5)
 print(p1.x) # 4
 print(p1.y) # 5
