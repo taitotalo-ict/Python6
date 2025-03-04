99779bfbcb533d14fe7d07705e27c08dad928ea7
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-03-04 11:12:06 +0200

    20250304_111206

--- GUI/harjoitus3.py
+++ GUI/harjoitus3.py
@@ -8,6 +8,7 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QColor
+from random import choice
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -18,21 +19,26 @@ class MainWindow(QMainWindow):
         self.show()
 
         COLORS = QColor.colorNames()
+        start_color = choice(COLORS)
 
         label = QLabel()
+        self.label = label
         label.setAutoFillBackground(True)
         # Tarran fontti
         font = label.font()
         font.setPointSize(30)
         label.setFont(font)
+        # label.setStyleSheet('font-size: 30px')
         # Tarran alignment
         label.setAlignment(Qt.AlignCenter)
+        # Tarran aloitusväri
+        self.update_label(start_color)
 
-        self.label = label
         combobox = QComboBox()
         # combobox.addItems(['red', 'green', 'blue'])
         combobox.addItems(COLORS)
         combobox.currentTextChanged.connect(self.update_label)
+        combobox.setCurrentText(start_color)
 
         layout = QVBoxLayout()
         layout.addWidget(label)
@@ -44,10 +50,11 @@ class MainWindow(QMainWindow):
 
     def update_label(self, color):
         self.label.setText(color)
-        palette = self.label.palette()
-        palette.setColor(self.label.backgroundRole(), color)
-        # palette.setColor(QPalette.Window, QColor(color))
-        self.label.setPalette(palette)
+        # palette = self.label.palette()
+        # palette.setColor(self.label.backgroundRole(), color)
+        # # palette.setColor(QPalette.Window, QColor(color))
+        # self.label.setPalette(palette)
+        self.label.setStyleSheet(f'background-color: {color}')
 
 
 app = QApplication()
