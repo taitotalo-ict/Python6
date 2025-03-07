244153c7a6e060e827e7ec5aa78070ed5c20c29d
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-03-07 14:01:37 +0200

    20250307_140137

--- APIt/nasa.py
+++ APIt/nasa.py
@@ -18,6 +18,17 @@ api_key = config['api_key']
 url = 'https://api.nasa.gov/planetary/apod'
 res = requests.get(url, params = {'api_key': api_key})
 if not res.ok:
+    print(res.reason)
+    raise Exception
+    
+
+image_url = res.json()['url']
+res = requests.get(image_url)
+if not res.ok:
+    print(res.reason)
     raise Exception
 
-pprint(res.json())
+image_filename = BASEDIR / image_url.split('/')[-1]
+
+with open(image_filename, 'wb') as f:
+    f.write(res.content)
