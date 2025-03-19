467c9f21e9211326f25e400d37c04187bc0df0bb
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-03-19 13:22:52 +0200

    20250319_132252

--- WebSovellukset/django_harjoittelu/django_project/urls.py
+++ WebSovellukset/django_harjoittelu/django_project/urls.py
@@ -15,11 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('hello/<str:name>/', views.hello),
+    path('', views.index, name='homepage'),
+    path('hello/<str:name>/', views.hello, name='hello'),
+    path('search/', views.search),
+    path('json/', views.json_view),
+    path('redirect/', views.redirect)
+    # path('products/', include('products.urls')) # projekti/products/urls.py
 ]
+
+
+
+# products/urls.py
+# urlpatterns = [
+#     path('', views.index, name='products_homepage'), # products/
+#     path('games/', views.index, name='products_games'), # products/games
+# ]
--- WebSovellukset/django_harjoittelu/django_project/views.py
+++ WebSovellukset/django_harjoittelu/django_project/views.py
@@ -1,7 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
 def index(request):
     return HttpResponse('Hello world!')
 
 def hello(request, name):
     return HttpResponse(f'Hello {name}')
+
+def search(request):
+    print(request.GET)
+    # query = request.GET['q']        # Exception jos 'q' ei olemassa
+    query = request.GET.get('q', 'oletusarvo')    # None jos 'q' ei olemassa
+    if not query:
+        return HttpResponse(f'Ok. Ei tullut mitään pyyntöä.')
+    else:
+        return HttpResponse(f'Ok. Yrität etsiä {query}')
+
+def json_view(request):
+    data = [{'key': 'value'}, {'name': 'Christian'}]
+    return JsonResponse(data, safe=False)
+
+def redirect(request):
+    return HttpResponseRedirect('/hello/christian')
