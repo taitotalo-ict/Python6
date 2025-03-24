880d8ede73948e8b399a31caea6ad83eaf3d6e14
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-03-24 10:56:05 +0200

    20250324_105605

--- Networking/basic_client.py
+++ Networking/basic_client.py
@@ -4,13 +4,16 @@ import socket
 # host = 'httpbin.org'
 # host = 'neverssl.com'
 host = 'www.thelegacy.de'
+# path = '/'
+path = '/6b48ceb067c10c5cc2aba0d5e6bead9d.jpg'
 port = 80
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((host, port))
 
-http_request = f'''GET /6b48ceb067c10c5cc2aba0d5e6bead9d.jpg HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\nUser-Agent: Test/0.1\r\n\r\n'''
+http_request = f'''GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\nUser-Agent: Test/0.1\r\n\r\n'''
 s.send(http_request.encode())
 
 while True:
