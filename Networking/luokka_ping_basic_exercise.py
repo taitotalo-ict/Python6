eeebf851bfa17764d196810d412f973c8f265c0c
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-03-26 11:43:43 +0200

    20250326_114343

--- Networking/luokka_ping_basic_exercise.py
+++ Networking/luokka_ping_basic_exercise.py
@@ -1,16 +1,22 @@
 # TITE luokka ping
-
+from threading import Thread
 
 def get_hosts_for_classroom(classroom:str) -> list:
     '''
     Creates and returns a list of hosts to ping for a classroom.
 
     The list contains tuples with the hostname and the ip address of the host: [(hostname, ip), ...].
-    Hostnames are in the format 'classroom-XXX' where XXX is a number from 001 to 020.
-    The ip addresses are in the format '172.20.XX.2YY' where XX is the classroom number (without the '5') and YY is a number from 001 to 020.
+    Hostnames are in the format '{classroom}-XXX' where XXX is a number from 001 to 020. For example: 521-014
+    The ip addresses are in the format '172.20.XX.2YY' where XX is the classroom number (without the '5') and YY is a number from 01 to 20.
     The last host is the OPE (kouluttajan) host. It's ip address ends in 200.
     '''
-    pass
+    lista = []
+    for i in range(1, 21):
+        hostname = f'{classroom}-{i:03}'
+        ip_address = f'172.20.{classroom[1:]}.2{i:02}'
+        lista.append((hostname, ip_address))
+    lista.append((f'{classroom}-OPE', f'172.20.{classroom[1:]}.200'))
+    return lista
 
 def ping(ip:str) -> bool:
     '''
@@ -25,7 +31,8 @@ def ping(ip:str) -> bool:
 
 def ping_host(hostname:str, ip:str) -> None:
     '''Ping a host and stores the response in the hosts_responses dictionary with the hostname as the key.'''
-    pass
+    responses[hostname] = ping(ip)
+    
 
 def print_responses() -> None:
     '''Prints the responses of the hosts in the hosts_responses dictionary in the format 'hostname: response'.'''
@@ -34,10 +41,14 @@ def print_responses() -> None:
 
 if __name__ == '__main__':
     # Get hosts to ping
+    hosts = get_hosts_for_classroom('521')
     
     # Initialize hosts_responses dictionary: {hostname: response, ...}
+    responses = {hostname: False for hostname, _ in hosts}
     
     # Launch ping threads
+    for hostname, ip in hosts:
+        Thread(target=ping_host, args=(hostname, ip)).start()
     
     # Wait for all pings to end
 
