335d3e212aa1c488831ad43b41cdf341500be17e
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-03-27 10:17:08 +0200

    20250327_101708

--- WebScraping/bs.py
+++ WebScraping/bs.py
@@ -1,9 +1,26 @@
-import requests
 import sys
+import requests
+from bs4 import BeautifulSoup
 
 res = requests.get('https://taitotalo.fi')
 
 if not res.ok:
     sys.exit(1)
 
-print(res.text)
+html_doc = res.text
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+# soup.h1
+# soup.p
+# link = soup.a
+# link.span
+# link['class']
+# link['href']
+# link.text
+# link.string
+# link.span.string
+# link.next
+# link.next.next
+# link.next_sibling
+
+
