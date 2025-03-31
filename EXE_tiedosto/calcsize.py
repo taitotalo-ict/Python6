0f0a4f47b36f0472ca5cdfcd4f53381b644cc9a3
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-03-31 13:21:48 +0300

    20250331_132148

--- EXE_tiedosto/calcsize.py
+++ EXE_tiedosto/calcsize.py
@@ -1,20 +1,22 @@
 from pathlib import Path
-import sys
+# import sys
+import argparse
+import logging
 
 
-# print(sys.argv)
-if len(sys.argv) > 1:
-    folder = Path(sys.argv[1])
-else:
-    folder = Path('.')
+# logger = logging.getLogger(__name__)
+logging.basicConfig(filename='calcsize.log', format='{asctime} - {levelname}:{message}', style='{', level=logging.INFO)
+logging.info('Starting script')
+
+parser = argparse.ArgumentParser(prog='calcsize', description='Calculates the size of a folder (with subdirectories)')
+parser.add_argument('folder', type=str, help='Path to the folder to calculate size for', default=Path('.'))
+args = parser.parse_args()
+
+folder = Path(args.folder)
+logging.info(f'Folder to calculate size: {folder.absolute()}')
+
 # folder = Path(sys.argv[1]) if len(sys.argv) > 1 else Path('.')
-summa = 0
-for item in folder.rglob('*'):
-    # print(item, end='')
-    if item.is_file():
-        # print(f' - Size: {item.stat().st_size}', end='')
-        summa += item.stat().st_size
-        # print(item.stat().st_size)
-    # print()
 
+summa = sum(item.stat().st_size for item in folder.rglob('*') if item.is_file())
 print(f'\nTotal size: {summa} bytes ({summa/1024/1024:.2f}MB)')
+logging.info('Ending script')
